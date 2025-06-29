1706a9a3f93d69db4df8d1617f9dd56a4a31a334
rehanp1 <rehanpathan8012@gmail.com>
2025-06-29 16:03:12 +0530

    Fixed fs module error

--- server/middlewares/multer.middleware.js
+++ server/middlewares/multer.middleware.js
@@ -1,8 +1,8 @@
 import multer from "multer";
-import path from "path"
-import fs from fs;
+import path from "path";
+import fs from "fs";
 
-const uploadPath = path.join('/tmp', 'uploads')
+const uploadPath = path.join("/tmp", "uploads");
 
 if (!fs.existsSync(uploadPath)) {
   fs.mkdirSync(uploadPath, { recursive: true });
